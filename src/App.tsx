4b9c5ec5acd47714e7349fc3f02536e381ab7644
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-02-06 20:45:48 -0300

    feat: dark mode e dropdownmenu no header

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { Toaster } from "sonner";
 
+import { ThemeProvider } from "./components/theme/theme-provider";
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors />
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzasho-theme" defaultTheme="dark">
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
