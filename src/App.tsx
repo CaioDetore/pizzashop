c493d74268daed0cc19836630714f07d0434101d
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-02-06 20:03:37 -0300

    feat: toast usando soonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
+import { Toaster } from "sonner";
 
 import { router } from "./routes";
 
 export function App() {
   return (
     <HelmetProvider>
+      <Toaster richColors />
       <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
     </HelmetProvider>
