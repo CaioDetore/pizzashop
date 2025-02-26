8ceefe5b531e5920acac005cdc5685a1af089775
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-02-26 20:41:57 -0300

    feat: loading orders

--- frontend/src/pages/app/orders/order-details.tsx
+++ frontend/src/pages/app/orders/order-details.tsx
@@ -20,6 +20,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import { OrderDetailsSkeleton } from "./order-details-skeleton";
+
 export interface OrderDetailsProps {
   orderId: string;
   open: boolean;
@@ -31,10 +33,6 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
     enabled: open,
   });
 
-  if (!order) {
-    return null;
-  }
-
   return (
     <DialogContent>
       <DialogHeader>
@@ -42,7 +40,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -139,6 +137,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   );
